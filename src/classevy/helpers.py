0cedb9a407a3c84e53c40aeb05bc2ce8a78dc214
Maarten Tytgat <maarten.tytgat@gmail.com>
2022-11-25 16:11:14 +0100

    added helper functions to determine optimal division per parameter

---
+++ src/classevy/helpers.py
@@ -0,0 +1,120 @@
+import numpy as np
+from typing import Union
+
+Numeric = Union[int, float, complex, np.number]
+
+
+# some general functions
+def next_best(options: list[int], choice: int) -> int:
+    """find the option that equals choice, or the next item or if there is no
+    next item, the first item
+
+    Args:
+        options: List of options to choose from.
+        choice: preferred option.
+
+    Returns:
+        choice or next item in the options or first item.
+    """
+    checks = [op >= choice for op in options]
+    idx = np.argmax(np.array(checks))
+    return options[idx]
+
+
+def hypo_spread(num: Numeric, sets: list[list[Numeric]]) -> list[Numeric]:
+    """Hypothetically assigning this num to one class or the other would result in
+    which new spreads?
+    We want to assign it to the option which causes the smallest new spread.
+
+    Args:
+        num: Input number to check.
+        sets: The list of sets versus which to check the num
+
+    Returns:
+        A list of hypothetical std across sets, for each set, if the num, were added to
+        that set.
+    """
+    sets = [k if len(k) else [0] for k in sets]
+    mean_stds = []  # will contain all possible std when the num is added to a class
+    for c in sets:
+        sets_no_c = sets.copy()
+        sets_no_c.remove(c)
+        mean_stds.append(
+            np.array(
+                [np.array(c0).mean() for c0 in sets_no_c] + [np.array(c + [num]).mean()]
+            ).std()
+        )
+    return mean_stds
+
+
+def biggest_impact(num_list: list[Numeric], sets: list[list[Numeric]]) -> Numeric:
+    """Which num in num_list has the biggest impact on the spread?
+    Pop it from the num_list.
+
+    Args:
+        num_list: List to pick the number from
+        sets: The list of sets versus which to check the num
+
+    Returns:
+        The value with the biggest impact on the spread of means of sets.
+    """
+    max_stds = []
+    for num in num_list:
+        mean_stds = hypo_spread(num, sets)
+        max_stds.append(np.max(np.array(mean_stds)))
+    index = np.argmax(np.array(max_stds))
+    return num_list.pop(index)
+
+
+def pop_absmax(lst: list[Numeric]) -> Numeric:
+    """Find the maximum absolute value in a list and pop it from the list.
+
+    Args:
+        lst: input list
+
+    Returns:
+        popped value
+    """
+    absmax = np.max(np.array([abs(i) for i in lst]))
+    if absmax in lst:
+        m = absmax
+    elif -absmax in lst:
+        m = -absmax
+
+    index = lst.index(m)
+    return lst.pop(index)
+
+
+def divide_list(
+    num_list: list[Numeric], n_sets: int
+) -> tuple[list[list[Numeric]], list[Numeric], Numeric]:
+    sets: list = [[] for _ in range(n_sets)]
+    for i in range(len(num_list)):
+
+        # first add the biggest impact numbers to the first available empty class
+        # note that this might mess up the standard deviation for small lists.
+        if any([len(c) == 0 for c in sets]) and False:
+            for c in sets:
+                if len(c) == 0:
+                    # num = pop_absmax(num_list)
+                    # print('num:', num)
+                    num = biggest_impact(num_list, sets)
+                    c.append(num)
+                    break
+
+        else:
+            # print('means', [np.array(c).mean() for c in sets])
+
+            # pick the number with the highest potential impact:
+            num = biggest_impact(num_list, sets)
+            # print('num:', num)
+            # print('hypo new means', [np.array(c+[num]).mean() for c in sets])
+            mean_stds = hypo_spread(num, sets)
+            # now add the num to the class that results in the smallest std:
+            # print('mean_stds:', mean_stds)
+            index = np.argmin(np.array(mean_stds))
+            # print('picking', index)
+            sets[index].append(num)
+    means = [np.array(c).mean() if len(c) else 0 for c in sets]
+    spread = np.array(means).std()
+    return sets, means, spread
---
+++ tests/helpers_unittest.py
@@ -0,0 +1,56 @@
+"""Unit tests for the helpers.py module"""
+import unittest
+from classevy import helpers
+
+
+class TestNextBest(unittest.TestCase):
+    """Unit tests for module-level functions."""
+
+    def test_next_best(self):
+        """Test the next_best function."""
+        options = [0, 1, 2, 4]
+        self.assertEqual(helpers.next_best(options, 0), 0)
+        self.assertEqual(helpers.next_best(options, 1), 1)
+        self.assertEqual(helpers.next_best(options, 2), 2)
+        self.assertEqual(helpers.next_best(options, 3), 4)
+        self.assertEqual(helpers.next_best(options, 5), 0)
+
+
+class TestPopAbsMax(unittest.TestCase):
+    """For the pop_absmax function"""
+
+    def test(self):
+        a = [1, -2]
+        absmax = helpers.pop_absmax(a)
+        self.assertEqual(absmax, -2)
+        self.assertEqual(a, [1])
+
+
+class TestDivide(unittest.TestCase):
+    """Unit tests for hypo spread, biggest_impact, divide_one_prop"""
+
+    def test_hypo_spread(self):
+        mean_stds = helpers.hypo_spread(1, [[-2, -2], [1, 1]])
+        self.assertEqual(mean_stds, [1.0, 1.5])
+
+    def test_biggest_impact(self):
+        bi = helpers.biggest_impact([10, 1, 0], [[0], [0]])
+        self.assertEqual(bi, 10)
+
+        bi = helpers.biggest_impact([10, 1, 0], [[10], [10]])
+        self.assertEqual(bi, 0)
+
+    def test_divide_list(self):
+        sets, means, spread = helpers.divide_list([1, 1, 1], 3)
+        self.assertEqual(sets, [[1], [1], [1]])
+        self.assertEqual(means, [1, 1, 1])
+        self.assertEqual(spread, 0)
+
+        # the algo will NOT favor filling empty lists first:
+        sets, means, spread = helpers.divide_list([-1, 1, 0], 3)
+        self.assertEqual(sets, [[-1, 1, 0], [], []])
+        self.assertEqual(means, [0, 0, 0])
+
+        sets, means, spread = helpers.divide_list([-1, 1, 0], 2)
+        self.assertEqual(sets, [[-1, 1, 0], []])
+        self.assertEqual(means, [0, 0])
